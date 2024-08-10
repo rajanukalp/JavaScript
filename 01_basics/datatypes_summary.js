336e3b0fd33acbfca41aad2cc2d132419cae4d7d
Anukalp Raj <raajanukalp@gmail.com>
2024-08-10 22:37:41 +0530

    Datatypes_summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -33,3 +33,36 @@ const myFunction = function(){
 }
 
 console.log(typeof anotherId);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++ Stack & Heap ++++++++++++++++++
+
+// Stack (primitive)&   Heap(Non-primitive)
+
+
+let useremail1 = "raajanukalp@gmail.com"
+let useremail2 = useremail1
+useremail2 = "anukalp2809@gmail.com"
+
+console.log(useremail1)
+console.log(useremail2)
+
+// in stack we get the copy, so if we have declare useremail2 = useremail1 we get the copy of useremail1 after we have declare useremail2 then we got the useremail2 output because main data of useremail1 didnot change.
+
+// Heap
+
+let user1 = {
+    email: "raajanukalp@gmail.com",
+    username1: "anukalpraj"
+
+}
+
+let user2 = user1
+
+user2.email = "rajanukalp2002@gmail.com"
+
+console.log(user1.email)
+console.log(user2.email)
+
+
+//+++++++ in heap we get the direct reference , so if we change change the value it will directly change to main data
