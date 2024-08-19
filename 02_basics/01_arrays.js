3a1fb42611883aeba221afa8ee7766ae1de63132
Anukalp Raj <raajanukalp@gmail.com>
2024-08-20 03:09:44 +0530

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,22 @@
+const newarray = [1, 2, 3, 4, 5];
+const array = new Array("anukalp", "raj");
+
+console.log(array)
+
+newarray.pop();
+newarray.push(6);
+
+
+console.log(newarray)
+
+const array1 = [1, 2, 3, 4, 5, 6];
+slicearray = array1.slice(1, 3);
+console.log(slicearray)
+
+console.log("A ", array1);
+
+splicearray = array1.splice(1,4);
+
+console.log(splicearray);
+console.log("B", array1)
+
