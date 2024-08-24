d084306fc49d72a91c2f2f0e6be9b075b07e4529
Anukalp Raj <raajanukalp@gmail.com>
2024-08-24 22:37:41 +0530

    function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -38,3 +38,42 @@ function userloginmessage(username) {
 
 console.log(userloginmessage("Anukalp"))
 
+
+function calculatecartvalue(...value1){
+
+    return value1
+}
+
+console.log(calculatecartvalue(100, 500, 10000))
+
+
+const userdata = {
+    name: "Anukalp",
+    email: "raajanukalp@gmail.com"
+}
+
+function handleobject(anyobject) {
+
+    console.log(`user name is ${anyobject.name} email is ${anyobject.email}`) // jo parameter function me declare kye h wo use krna h
+}
+
+handleobject(userdata)     // kaha compulsory object name hee dalna hai
+
+handleobject({
+    name: "ritik",
+    email: "ritik@gmail.com"
+
+})                                    // is trah se v object declare kr skte h
+
+
+const myarray = ["Anukalp", "Rahul", "Abhinav", "Ritik"]
+
+function getthirdvalue(anyarray){
+    console.log(anyarray[2])
+    
+}
+getthirdvalue(myarray)
+
+getthirdvalue(["Anukalp", "Rahul", "Abhinav", "Ritik"])
+
+
